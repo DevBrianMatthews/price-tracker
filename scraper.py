1fde2e953d8a2eb5927208cd4c6420d1dc450185
BrianMatthews <brianmatthews.contact@gmail.com>
2026-03-19 17:41:17 -0500

    Test script tha retrieves a product's price in scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,23 @@
 
 # Archivo de código de prueba
+import requests
+from bs4 import BeautifulSoup
 
+
+# URL del sitio a busar el precio
+url = 'https://www.alkosto.com/macbook-pro-14-pulgadas-chip-m5-cpu-10-nucleos-gpu-10/p/195950488876?algEvent=eyJvYmplY3RJZCI6IjE5NTk1MDQ4ODg3NiIsImluZGV4IjoiYWxrb3N0b0luZGV4QWxnb2xpYVBSRCIsImFjdGlvbiI6InZpZXciLCJxdWVyeUlEIjoiOTU0MzhkYzUyMTUzZWUwOTk5MzljMGUxMTQ3NzdlNzQifQ=='
+
+# Traer el HTML completo del sitio web
+request = requests.get(url)
+html    = request.text
+
+# Buscar dentro de ese HTML la etiqueta donde está el precio
+soup    = BeautifulSoup(html, 'html.parser')
+element = soup.find('span', id="js-original_price")
+
+# Traer el precio y limpiarlo
+price      = element.text
+clean_text = price.strip().split(" ")
+
+# Mostrar el precio limpio en consola
+print(clean_text[0])
