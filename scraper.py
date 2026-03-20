083516f21b1c007ae1990bccde495fa6fc40978c
BrianMatthews <brianmatthews.contact@gmail.com>
2026-03-19 21:45:32 -0500

    Add script to display test prouct name and price.

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+import sqlite3
+
 
 # URL del sitio a busar el precio
 url = 'https://www.alkosto.com/macbook-pro-14-pulgadas-chip-m5-cpu-10-nucleos-gpu-10/p/195950488876?algEvent=eyJvYmplY3RJZCI6IjE5NTk1MDQ4ODg3NiIsImluZGV4IjoiYWxrb3N0b0luZGV4QWxnb2xpYVBSRCIsImFjdGlvbiI6InZpZXciLCJxdWVyeUlEIjoiOTU0MzhkYzUyMTUzZWUwOTk5MzljMGUxMTQ3NzdlNzQifQ=='
@@ -16,8 +18,24 @@ soup    = BeautifulSoup(html, 'html.parser')
 element = soup.find('span', id="js-original_price")
 
 # Traer el precio y limpiarlo
-price      = element.text
-clean_text = price.strip().split(" ")
+gross_price = element.text
+price       = gross_price.strip().split(" ")
 
 # Mostrar el precio limpio en consola
-print(clean_text[0])
+print(price[0])
+
+
+soup_name    = BeautifulSoup(html, 'html.parser')
+element_name = soup_name.find('h1', class_="js-main-title")
+name         = element_name.text
+
+print(name)
+
+# ----------- DATA BASE ----------
+
+# conection = sqlite3.connect('prices.db')
+
+# cursor = conection.cursor()
+
+# cursor.execute('CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, name TEXT, url TEXT)')
+
