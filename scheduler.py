a785fe4906772e0e1c4ef12ba21d5c273672fcfb
BrianMatthews <brianmatthews.contact@gmail.com>
2026-05-13 09:53:15 -0500

    Add Falabella scraper and fix database connection closing inside loop.

--- scheduler.py
+++ scheduler.py
@@ -1,41 +1,57 @@
-from database.models import get_connection, create_tables, insert_product, get_product_id, get_last_price, insert_price
+from database.models             import get_connection, create_tables, insert_product, get_product_id, get_last_price, insert_price
 from notifiers.telegram_notifier import send_notification
-from scrapers.alkosto import AlkostoScraper
+from scrapers.alkosto            import AlkostoScraper
+from scrapers.falabella          import FalabellaScraper
 import asyncio
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-url = 'https://www.alkosto.com/macbook-pro-14-pulgadas-chip-m5-cpu-10-nucleos-gpu-10/p/195950488876?algEvent=eyJvYmplY3RJZCI6IjE5NTk1MDQ4ODg3NiIsImluZGV4IjoiYWxrb3N0b0luZGV4QWxnb2xpYVBSRCIsImFjdGlvbiI6InZpZXciLCJxdWVyeUlEIjoiOTU0MzhkYzUyMTUzZWUwOTk5MzljMGUxMTQ3NzdlNzQifQ=='
+products = [
+    {
+        "url": 'https://www.alkosto.com/macbook-pro-14-pulgadas-chip-m5-cpu-10-nucleos-gpu-10/p/195950489347',
+        "scraper": AlkostoScraper()
+    },
+    {
+        "url":'https://www.falabella.com.co/falabella-co/product/73317218/TCL-55-pulgadas-4K-UHD-QLED-55C6K/73317218',
+        "scraper": FalabellaScraper()
+    }
+]
+
 
 def check_prices():
     # Database
     conection, cursor = get_connection('prices.db')
     create_tables(cursor)
 
-    # Scraping
-    scraper = AlkostoScraper()
-    html    = scraper.fetch_page(url)
-    price   = scraper.get_price(html)
-    name    = scraper.get_name(html)
 
-    # Save the product and retrieve its ID
-    insert_product(cursor, name, url)
-    product_id = get_product_id(cursor, url)
+    for product in products:
+        url     = product["url"]
+        scraper = product["scraper"]
 
-    # Compare prices
-    last_price = get_last_price(cursor, product_id)
+        # Scraping
+        html    = scraper.fetch_page(url)
+        price   = scraper.get_price(html)
+        name    = scraper.get_name(html)
 
-    if last_price is None:
-        print(f'Primer registro, precio actual: {price}')
-    elif price < last_price[0]:
-        message = f'El precio de {name} bajó, ahora está en: ${price:,.0f}\n{url}'
-        asyncio.run(send_notification(message))
+        # Save the product and retrieve its ID
+        insert_product(cursor, name, url)
+        product_id = get_product_id(cursor, url)
 
-    # Always save the current price
-    insert_price(cursor, product_id, price)
-    conection.commit()
+        # Compare prices
+        last_price = get_last_price(cursor, product_id)
+
+        if last_price is None:
+            print(f'Primer registro, precio actual: {price}')
+        elif price < last_price[0]:
+            message = f'El precio de {name} bajó, ahora está en: ${price:,.0f}\n{url}'
+            asyncio.run(send_notification(message))
+
+        # Always save the current price
+        insert_price(cursor, product_id, price)
+        conection.commit()
+
     conection.close()
 
 scheduler = BlockingScheduler()
-scheduler.add_job(check_prices, 'interval', seconds=10)
+scheduler.add_job(check_prices, 'interval', seconds=15)
 scheduler.start()
--- scrapers/falabella.py
+++ scrapers/falabella.py
@@ -1,19 +1,19 @@
 from scrapers.base_scraper import BaseScraper
 from bs4                   import BeautifulSoup
-class AlkostoScraper(BaseScraper):
+
+
+class FalabellaScraper(BaseScraper):
     def get_price(self, html):
         soup    = BeautifulSoup(html, 'html.parser')
-        element = soup.find('span', id="js-original_price")
+        element = soup.find('span', class_="copy12")
 
         gross_price = element.text
-        price       = gross_price.strip().split(" ")[0]
-        price       = price.replace("$", "").replace(".", "")
-        price       = float(price)
+        price       = float(gross_price.strip().replace('$', '').replace('.', '').strip())
 
         return price
 
     def get_name(self, html):
         soup         = BeautifulSoup(html, 'html.parser')
-        element_name = soup.find('h1', class_="js-main-title")
+        element_name = soup.find('h1', class_="pdp-basic-info__product-name")
         name         = element_name.text
         return name
